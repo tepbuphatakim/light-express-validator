16aec93d769f78363c110abee90cac0c7c58cba2
tepbuphata.kim <kimtepbuphata009@gmail.com>
2024-09-10 21:28:42 +0700

    test: required fields error response

--- tests/validator.test.js
+++ tests/validator.test.js
@@ -10,12 +10,12 @@ describe('light-express-validator', () => {
     app.use(express.json());
   });
 
-  test('should fail validation for missing required field', async () => {
+  test('should fail with required field', async () => {
     app.post(
       '/test',
       validate({
         name: 'required',
-        email: 'required',
+        email: '',
       }),
       (req, res) => {
         res.status(200).json({ message: 'Validation passed' });
@@ -24,11 +24,9 @@ describe('light-express-validator', () => {
 
     const response = await request(app)
       .post('/test')
-      .send({ name: 'John Doe', email: '' });
-    console.debug({ response: response });
-    expect(response.headers['content-type']).toContain('application/json');
+      .send({ name: '', email: '' });
 
     expect(response.status).toBe(400);
-    expect(response.body.error).toBe('The email field is required.');
+    expect(response.body.fields.name).toBe('The name field is required.');
   });
 });
